9335aa65f41f14e8fc13d2f742b06323daceac1b
spacexerq <bns2002@yandex.ru>
2023-11-23 20:31:35 +0300

    complete release

--- ht1/scalar_mul.cpp
+++ ht1/scalar_mul.cpp
@@ -1,37 +1,34 @@
 #include <iostream>
-#include <string>
 #include <emmintrin.h>
 #include <xmmintrin.h>
 #include <cmath>
+#include <chrono>
 
 using namespace::std;
 /*using this guide*/
 /*http://ccfit.nsu.ru/~kireev/lab4/lab4intr.htm*/
 
 
-float dot_sse(const float* x, const float* y, const long& len) {
+float dot_sse(float* x, float* y, int& len) {
     float inner_prod = 0;
     __m128 xn, yn;
-    __m128 acc = _mm_setzero_ps(); // Declare a variable stored in a 128-bit special register in SSE to store the result of X+Y, the initial value is 0
-    float temp[4];//Store the parameters of the intermediate variables
-
-    long i;
-    for (i = 0; i + 4 <
-                len; i += 4) {//128-bit special register, which can handle 4 groups of 32-bit variable operations at one time
-        xn = _mm_loadu_ps(x +
-                          i); // Load x to X (because 128 bits can store four 32-bit data, the default is to load 4 consecutive parameters at a time)
-        yn = _mm_loadu_ps(y + i);//Same as above
-        acc = _mm_add_ps(acc, _mm_mul_ps(xn,
-                                         yn));//x*y, the sum of x1*y1 in each round, the sum of x2*y2, the sum of x3*y3, the sum of x4*y4, and the final result The four sums are placed in the 128-bit register of acc.
+    __m128 prod = _mm_setzero_ps();
+    float temp[4];
+    int i = 0;
+    for (i = 0; i + 4 < len; i += 4) // 128/4 = 32 bit
+    {
+        xn = _mm_loadu_ps(x+i);
+        yn = _mm_loadu_ps(y+i);
+        __m128 temp_sum = _mm_mul_ps(xn, yn);
+        prod = _mm_add_ps(prod, temp_sum);
     }
-    _mm_storeu_ps(&temp[0], acc); // Load 4 32-bit data in acc into memory
-    inner_prod = temp[0] + temp[1] + temp[2] + temp[3];//dot multiplication sum
+    _mm_storeu_ps(&temp[0], prod);
+    inner_prod = temp[0] + temp[1] + temp[2] + temp[3];
 
-    // Just processed the dot multiplication of elements that are multiples of the first 4. If len is not a multiple of 4, then there is a little tail to deal with
     for (; i < len; ++i) {
-        inner_prod += x[i] * y[i];//Continue to accumulate the product of the little tail
+        inner_prod += x[i] * y[i];
     }
-    return inner_prod;//You're done
+    return inner_prod;
 }
 
 float dot(float* x, float* y, int n)
@@ -52,20 +49,30 @@ bool compare(float a, float b){
 }
 
 int main(){
-    int scale_sample = 10000;
+    int scale_sample = 100000;
     srand(time(0));
     float x[scale_sample], y[scale_sample];
     for(int i = 0; i<scale_sample;i++){
         x[i]=int(rand()/10e3);
         y[i]=int(rand()/10e3);
     }
+    auto start_default = chrono::steady_clock::now();
     float scal_pr = dot(x,y,scale_sample);
+    auto end_default = chrono::steady_clock::now();
+    auto start_sse = chrono::steady_clock::now();
     float scal_pr_sse = dot_sse(x,y,scale_sample);
-    cout << scal_pr << endl;
-    cout << scal_pr_sse << endl;
-    if(compare(scal_pr, scal_pr_sse)){
-        cout << "Numerical mistake";
+    auto end_sse = chrono::steady_clock::now();
+    //cout << scal_pr << endl;
+    //cout << scal_pr_sse << endl;
+    auto seconds_default = end_default - start_default;
+    auto seconds_sse = end_sse - start_sse;
+    if(not compare(scal_pr, scal_pr_sse)){
+        cout << "Numerical mistake" << endl;
+        return -1;
     }else{
-        cout << "Production complete";
+        cout << "Production complete" << endl;
     }
+    cout << "Time spent for " << scale_sample << " vector length is:" << endl;
+    cout << "By SSE:" << chrono::duration<double>(seconds_sse).count() << endl;
+    cout << "By default:" << chrono::duration<double>(seconds_default).count() << endl;
 }
--- ht1/square_root.cpp
+++ ht1/square_root.cpp
@@ -1,18 +1,63 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <xmmintrin.h>
-#include <math.h>
+#include <cmath>
+#include <chrono>
 
 using namespace::std;
 
 
-void normal(float* a, float* b, int N)
+void default_sqrt(float* a, float* b, int N)
 {
     for (int i = 0; i < N; ++i)
         b[i] = sqrt(a[i]);
 }
 
-int main(){
+void sse_sqrt(float* a, float* b, int N){
+    int i = 0;
+    for (i = 0; i + 4 < N; i += 4) // 128/4 = 32 bit
+    {
+        __m128 x = _mm_load_ps(&a[i]);
+        __m128 res = _mm_sqrt_ps((__m128) x);
+        _mm_store_ps(&b[i], res);
+    }
+    for (; i < N; ++i) {
+        b[i] = sqrt(a[i]);
+    }
+}
 
+bool compare(float* a, float* b, int N){
+    for(int i = 0; i<N; i++){
+        if(a[i]!=b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int scale_sample = 100004;
+    srand(time(0));
+    float x[scale_sample], res_sse[scale_sample], res_def[scale_sample];
+    for(int i = 0; i<scale_sample;i++){
+        x[i]=int(rand()/10e3);
+    }
+    auto start_default = chrono::steady_clock::now();
+    default_sqrt(x,res_def,scale_sample);
+    auto end_default = chrono::steady_clock::now();
+    auto start_sse = chrono::steady_clock::now();
+    sse_sqrt(x,res_sse,scale_sample);
+    auto end_sse = chrono::steady_clock::now();
+    //cout << scal_pr << endl;
+    //cout << scal_pr_sse << endl;
+    auto seconds_default = end_default - start_default;
+    auto seconds_sse = end_sse - start_sse;
+    if(not compare(res_sse, res_def, scale_sample)){
+        cout << "Numerical mistake" << endl;
+        return -1;
+    }else{
+        cout << "Calculation complete" << endl;
+    }
+    cout << "Time spent for " << scale_sample << " vector length is:" << endl;
+    cout << "By SSE:" << chrono::duration<double>(seconds_sse).count() << endl;
+    cout << "By default:" << chrono::duration<double>(seconds_default).count() << endl;
 }
